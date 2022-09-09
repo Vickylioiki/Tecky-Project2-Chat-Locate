fbdb10e02d63418a0899259860059e0220d2ddf8
Vicky Li <vickylioiki@gmail.com>
2022-09-09 11:37:28 +0800

    express

---
+++ main.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json());
+
+
+
+
+app.listen(8080, () => {
+
+    console.log('Server is listening http://localhost:8080');
+
+})
