395f4433d2405be1de6095b6d0331b83e56baf49
Vicky Li <vickylioiki@gmail.com>
2022-09-09 11:42:52 +0800

    import session

--- main.ts
+++ main.ts
@@ -1,13 +1,33 @@
-import express from 'express'
+import express from 'express';
+import expressSession from 'express-session';
+
 
 const app = express()
 app.use(express.json());
+
+//Session
+app.use(
+  expressSession({
+    secret: 'Tecky Academy teaches typescript',
+    resave: true, //Auto save session 
+    saveUninitialized: true,
+  }),
+)
+
+
+declare module 'express-session' {
+  interface SessionData {
+    name?: string
+  }
+}
+
+
 
 
 
 
 app.listen(8080, () => {
 
-    console.log('Server is listening http://localhost:8080');
+  console.log('Server is listening http://localhost:8080');
 
 })
