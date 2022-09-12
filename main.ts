c603ad26a3eaa4507556ac55b0a43a22f038f3ca
yah30022000 <103918223+yah30022000@users.noreply.github.com>
2022-09-12 14:08:23 +0800

    added login and registration logic

--- main.ts
+++ main.ts
@@ -1,9 +1,33 @@
-import express from 'express';
+import { Client } from 'pg'
+import express from 'express'
 import expressSession from 'express-session'
+import http from 'http'
+import { Server as SocketIO } from 'socket.io'
+import { userRoutes } from './routes/userRoute'
+import grant from 'grant'
 
+import dotenv from 'dotenv'
+dotenv.config()
+
+declare module 'express-session' {
+  interface SessionData {
+    name?: string
+    isloggedin?: boolean
+  }
+}
 
 const app = express()
+
+const server = new http.Server(app)
+export const io = new SocketIO(server)
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+export const client = new Client({
+  database: process.env.DB_NAME,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD
+})
+client.connect()
 
 //Session
 app.use(
@@ -14,13 +38,41 @@ app.use(
   }),
 )
 
+app.use('/user', userRoutes)
+app.use(express.static('public'))
 
-declare module 'express-session' {
-  interface SessionData {
-    name?: string
+const grantExpress = grant.express({
+  defaults: {
+    origin: 'http://localhost:8080',
+    transport: 'session',
+    state: true
+  },
+  google: {
+    key: process.env.GOOGLE_CLIENT_ID || '',
+    secret: process.env.GOOGLE_CLIENT_SECRET || '',
+    scope: ['profile', 'email'],
+    callback: '/user/login/google'
+  },
+  facebook: {
+
+    key: process.env.FACEBOOK_CLIENT_ID || '',
+    secret: process.env.FACEBOOK_CLIENT_SECRET || '',
+    scope: ['public_profile', 'email'],
+    callback: '/user/login/facebook',
+    redirect_uri: 'http://localhost:8080/connect/facebook/callback'
+  },
+  instagram: {
+    key: process.env.INSTAGRAM_CLIENT_ID || '',
+    secret: process.env.INSTAGRAM_CLIENT_SECRET || '',
+    scope: ['user_profile', 'user_media'],
+    callback: '/user/login/instagram',
+    redirect_uri: 'https://google.com/'
   }
-}
 
+}
+)
+
+app.use(grantExpress as express.RequestHandler)
 
 
 
---
+++ routes/userRoute.ts
@@ -0,0 +1,263 @@
+import express from 'express'
+import { client } from '../main'
+import { checkPassword, hashPassword } from '../hash'
+import fetch from 'cross-fetch'
+import crypto from 'crypto'
+
+export const userRoutes = express.Router()
+
+userRoutes.get('/', async (req, res) => {
+    let userResult = await client.query('select * from users')
+    res.json(userResult.rows)
+})
+
+userRoutes.post('/register', async (req, res) => {
+    try {
+        const { username, password, name } = req.body
+        console.log('req.body', req.body)
+        if (!username || !password || !name) {
+            res.status(400).json({
+                message: 'Invalid username or password'
+            })
+            return
+        }
+        console.log('username and password checking passed!!')
+
+        let userResult = await client.query(
+            `select * from users where username = $1`,
+            [username]
+        )
+
+        if (userResult.rows.length > 0) {
+            res.status(400).json({
+                message: 'username already exists'
+            })
+            return
+        }
+        console.log('existing username checking passed!!')
+
+        let hashedPassword = await hashPassword(password)
+        console.log('hashedPassword', hashedPassword)
+        await client.query(
+            `insert into users (username, password) values ($1, $2)`,
+            [username, hashedPassword]
+        )
+        res.json({ message: 'User created' })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: 'Internal server error' })
+    }
+})
+
+userRoutes.post('/login', async (req, res) => {
+    console.log('userRoutes - /login', req.body)
+    const { username, password } = req.body
+    if (!username || !password) {
+        res.status(400).json({
+            message: 'Invalid username or password'
+        })
+        return
+    }
+    console.log('/login-username and password checking passed!!')
+
+    let userResult = await client.query(
+        `select * from users where username = $1`,
+        [username]
+    )
+    let dbUser = userResult.rows[0]
+
+    if (!dbUser) {
+        res.status(400).json({
+            message: 'Invalid username'
+        })
+        return
+    }
+    console.log('/login-existing username checking passed!!')
+
+    let isMatched = await checkPassword(password, dbUser.password)
+    if (!isMatched) {
+        res.status(400).json({
+            message: 'Invalid username or password'
+        })
+        return
+    }
+    console.log('/login-valid password checking passed!!')
+
+    let {
+        password: dbUserPassword,
+        id,
+        created_at,
+        updated_at,
+        ...sessionUser
+    } = dbUser
+    req.session['user'] = sessionUser
+
+    // console.log(sessionUser)
+    res.status(200).json({
+        message: 'Success login'
+    })
+})
+
+userRoutes.get('/logout', (req, res) => {
+    req.session.destroy(() => {
+        console.log('user logged out')
+    })
+    res.redirect('/')
+})
+
+userRoutes.get('/login/google', loginGoogle);
+
+async function loginGoogle(req: express.Request, res: express.Response) {
+    const accessToken = req.session?.['grant'].response.access_token
+
+    console.log('accessToken', accessToken)
+
+    const fetchRes = await fetch(
+        'https://www.googleapis.com/oauth2/v2/userinfo',
+        {
+            method: 'get',
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        }
+    )
+    const googleProfile = await fetchRes.json()
+    console.log(googleProfile)
+
+    const users = (
+        await client.query(`SELECT * FROM users WHERE username = $1`, [
+            googleProfile.email
+        ])
+    ).rows
+    let user = users[0]
+
+    if (!user) {
+        const randomString = crypto.randomBytes(32).toString('hex')
+        let hashedPassword = await hashPassword(randomString)
+
+        user = (
+            await client.query(
+                `INSERT INTO users (username,password)
+                VALUES ($1, $2) RETURNING *`,
+                [googleProfile.email, hashedPassword]
+            )
+        ).rows[0]
+        console.log(user)
+        await client.query(
+            `INSERT INTO google_profile(user_id,profile_pic,google_id,name)
+            VALUES ($1, $2, $3, $4) RETURNING *`,
+            [user.id, googleProfile.picture, googleProfile.id, googleProfile.name]
+        )
+    }
+
+
+
+    if (req.session) {
+        req.session['user'] = googleProfile
+    }
+    res.redirect('/')
+}
+userRoutes.get('/login/facebook', loginfacebook);
+async function loginfacebook(req: express.Request, res: express.Response) {
+    const accessToken = req.session?.['grant'].response.access_token
+
+    console.log('accessToken:', accessToken)
+
+    // retrieve user ID
+    const userIdRes = await fetch(
+        `https://graph.facebook.com/me?access_token=${accessToken}`,
+
+    )
+
+    const facebookuserId = await userIdRes.json()
+    console.log('facebookuserId: ', facebookuserId)
+
+    const userfields = await fetch(
+        `https://graph.facebook.com/${facebookuserId.id}?fields=id,name,email,picture&access_token=${accessToken}`
+    )
+    const facebookProfile = await userfields.json()
+    console.log('facebookProfile', facebookProfile)
+
+    const users = (
+        await client.query(`SELECT * FROM users WHERE username = $1`, [
+            facebookProfile.email
+        ])
+    ).rows
+    let user = users[0]
+
+    if (!user) {
+        const randomString = crypto.randomBytes(32).toString('hex')
+        let hashedPassword = await hashPassword(randomString)
+
+        user = (
+            await client.query(
+                `INSERT INTO users (username,password)
+                VALUES ($1, $2) RETURNING *`,
+                [facebookProfile.email, hashedPassword]
+            )
+        ).rows[0]
+        console.log(user)
+        await client.query(
+            `INSERT INTO facebook_profile(user_id,profile_pic,fb_id,name)
+            VALUES ($1, $2, $3, $4) RETURNING *`,
+            [user.id, facebookProfile.picture, facebookProfile.id, facebookProfile.name]
+        )
+    }
+    if (req.session) {
+        req.session['user'] = facebookProfile
+    }
+    res.redirect('/')
+}
+
+userRoutes.get('/login/instagram', logininstagram);
+async function logininstagram(req: express.Request, res: express.Response) {
+    const accessToken = req.session?.['grant'].response.access_token
+
+    console.log('accessToken:', accessToken)
+
+    // retrieve user ID
+    const userIdRes = await fetch(
+        `https://graph.instagram.com/me?access_token=${accessToken}`,
+
+    )
+
+    const instagramuserId = await userIdRes.json()
+    console.log('instagram: ', instagramuserId)
+
+    const userfields = await fetch(
+        `https://graph.instagram.com/${instagramuserId.id}?fields=id,username&access_token=${accessToken}`
+    )
+    const instagramProfile = await userfields.json()
+    console.log('instagramProfile', instagramProfile)
+
+    const users = (
+        await client.query(`SELECT * FROM users WHERE username = $1`, [
+            instagramProfile.email
+        ])
+    ).rows
+    let user = users[0]
+
+    if (!user) {
+        const randomString = crypto.randomBytes(32).toString('hex')
+        let hashedPassword = await hashPassword(randomString)
+
+        user = (
+            await client.query(
+                `INSERT INTO users (username,password)
+                VALUES ($1, $2) RETURNING *`,
+                [instagramProfile.email, hashedPassword]
+            )
+        ).rows[0]
+        console.log(user)
+
+        await client.query(
+            `INSERT INTO instagram_profile(user_id,profile_pic,ig_id,name,media_count)
+                VALUES ($1, $2, $3, $4, $5) RETURNING *`,
+            [user.id, instagramProfile.picture, instagramProfile.id, instagramProfile.name, instagramProfile.media_count]
+        )
+    }
+    if (req.session) {
+        req.session['user'] = instagramProfile
+    }
+    res.redirect('/')
+}
