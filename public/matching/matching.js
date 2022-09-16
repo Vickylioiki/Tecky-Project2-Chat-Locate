d814e7cc91108e92ba3c70f3a9ec1585f7a28b77
Vicky Li <vickylioiki@gmail.com>
2022-09-16 11:09:55 +0800

    match

--- public/matching/matching.js
+++ public/matching/matching.js
@@ -1,6 +1,11 @@
 async function init() {
+    console.log(1.1);
     await getLocation();
-    // initMap()
+    console.log(1.2);
+    await initMap();
+    console.log(1.3);
+    await getNewUser()
+
 }
 
 
@@ -25,14 +30,14 @@ async function getLocation() {
     })
 }
 
-// init();
 
 
-// async function getReadyUsers() {
+// async function geteadyUsers() {
 //     const res = await fetch('/match');
 //     const readyUsers = await res.json();
 //     console.log(readyUsers);
 // }
+
 
 
 async function initMap() {
@@ -46,39 +51,20 @@ async function initMap() {
     let owner = readyUsers.filter(readyUser => {
         return readyUser.userId == userId
     })[0]
-    console.log(owner)
-    //NO USE//
-    // const bounds = new google.maps.LatLngBounds();
-    // const markersArray = [];
 
-    // const map = new google.maps.Map(
-    //     document.getElementById("map"),
-    //     {
-    //         center: { lat: 55.53, lng: 9.4 },
-    //         zoom: 10,
-    //     }
-    // );
+
+    console.log('owner:', owner)
 
     // initialize services
     const geocoder = new google.maps.Geocoder();
     const service = new google.maps.DistanceMatrixService();
-    console.log(readyUsers);
-
-    // build request
-
-    // const origin1 = { lat: 55.93, lng: -3.118 };
-    // const origin2 = "Greenwich, England";
-    // const destinationA = "Stockholm, Sweden";
-    // const destinationB = { lat: 50.087, lng: 14.421 };
-    // for (let i = 0; i < readyUsers.length; i++) {
-    //     let originUser = readyUsers[i].location;
+    console.log('readyUsers.length:', readyUsers.length)
     let distances = []
     for (let j = 0; j < readyUsers.length; j++) {
         if (readyUsers[j].userId == owner.userId) {
             continue;
         }
         console.log("j :" + j);
-
         let destinationUser = readyUsers[j].location;
         // console.log(`destinationUser${j}`, destinationUser)
         const request = {
@@ -93,17 +79,15 @@ async function initMap() {
         distances.push({
             userId: readyUsers[j].userId,
             distance: response.rows[0].elements[0].distance.value
-        })
-        // .then((response) => {
-        //     console.log(`response${j}`, destinationUser)
 
-        //     // const result = response.rows[0]
-        //     console.log(response);
-        // })
+
+        })
     }
+
     distances = distances.sort((a, b) => {
         return a.distance - b.distance
     })
+
     console.log(`distances`, distances)
     const startChatRes = await fetch('/match/startChat', {
         method: 'POST',
@@ -118,81 +102,23 @@ async function initMap() {
     const startData = await startChatRes.json()
     console.log(`startChatRes`, startData)
 
-    // }
+
+
+
 }
-// const request = {
-//     origins: [readyUsers[0].location],
-//     destinations: [readyUsers[1].location, readyUsers[2].location],
-//     travelMode: google.maps.TravelMode.WALKING,
-//     unitSystem: google.maps.UnitSystem.METRIC
-// };
-
-// put request on page
-// document.getElementById("request").innerText =
-//     JSON.stringify(request, null, 2);
-
-// get distance matrix response
-// service.getDistanceMatrix(request).then((response) => {
-// put response
-// document.getElementById("response").innerText =
-//     JSON.stringify(response, null, 2);
-
-// show on map
-// const originList = response.originAddresses;
-// const destinationList = response.destinationAddresses;
-// const userAdistance = ('sender:', response.originAddresses)
 
 
 
 
-// console.log(response)
+async function getNewUser() {
+    console.log(4);
+    const checkRemainUsers = await fetch('/match');
+    let readyUsers = await checkRemainUsers.json();
+    console.log(`readyUsers2`, readyUsers)
+}
 
-
-// deleteMarkers(markersArray);
-
-// const showGeocodedAddressOnMap = (asDestination) => {
-//     const handler = ({ results }) => {
-//         map.fitBounds(bounds.extend(results[0].geometry.location));
-//         markersArray.push(
-//             new google.maps.Marker({
-//                 map,
-//                 position: results[0].geometry.location,
-//                 label: asDestination ? "D" : "O",
-//             })
-//         );
-//     };
-
-//     return handler;
-// };
-
-// for (let i = 0; i < originList.length; i++) {
-//     const results = response.rows[i].elements;
-
-//     geocoder
-//         .geocode({ address: originList[i] })
-//         .then(showGeocodedAddressOnMap(false));
-
-//     for (let j = 0; j < results.length; j++) {
-//         geocoder
-//             .geocode({ address: destinationList[j] })
-//             .then(showGeocodedAddressOnMap(true));
-//     }
-// }
-// async function distributeReadyUsers() {
-//     const res = await fetch('/match');
-//     let readyUsers = await res.json();
-//     if (readyUsers.length > 1) {
-//         initMap();
-//     }
-
-
-// }
-
-
-// function main() {
-//     distributeReadyUsers();
-
-// }
-
+console.log(1);
 init();
-main();
+console.log(2);
+// getNewUser();
+console.log(3);
--- routes/matchRoute.ts
+++ routes/matchRoute.ts
@@ -3,50 +3,72 @@ import express from 'express'
 
 
 let readyUsers: any = [];
-
-// { userId: 1, location: { lat: 22.285170575820224, lng: 114.14665642394633 } }, { userId: 2, location: { lat: 22.283651333323313, lng: 114.15176410872265 } }, { userId: 3, location: { lat: 22.28601222944395, lng: 114.14757727141927 } }, { userId: 4, location: { lat: 22.286290196846565, lng: 114.14976595398261 } }
-//     // const x = [22.285170575820224, 114.14665642394633] // 東華醫院 (12 Po Yan St, Sai Wan, Hong Kong)
+// { userId: 4, location: { lat: 22.286290196846565, lng: 114.14976595398261 } }, { userId: 1, location: { lat: 22.285170575820224, lng: 114.14665642394633 } }, { userId: 2, location: { lat: 22.30933514419831, lng: 114.23787345976665 } }
+// { userId: 1, location: { lat: 22.285170575820224, lng: 114.14665642394633 } }, { userId: 2, location: { lat: 22.30933514419831, lng: 114.23787345976665} }, { userId: 3, location: { lat: 22.28601222944395, lng: 114.14757727141927 } }, { userId: 4, location: { lat: 22.286290196846565, lng: 114.14976595398261 } }
+//     // const x = [22.30933514419831, 114.23787345976665] // Lam Tin 
 //     // const y = [22.2836612609074, 114.15163536268929] // 元創方 (H211 Block B PMQ, 35 Aberdeen St, Central, Hong Kong)
-//     // const y = df[22.28601222944395, 114.14757727141927] // 荷李活道公園 (Hong Kong, Tai Ping Shan, 荷李活道228號B)
+//     // const y = [22.28601222944395, 114.14757727141927] // 荷李活道公園 (Hong Kong, Tai Ping Shan, 荷李活道228號B)
 //     // const y = [22.286290196846565, 114.14976595398261] 上環文娛中心 (345 Queen's Road Central, Sheung Wan, Hong Kong)
 
 export const matchRoutes = express.Router()
 
+
+
+
+//Matched Users
 matchRoutes.post('/startChat', (req, res) => {
-    const currentUserId = req.body.currentUserId;
-    const userId = req.body.userId
-    console.log(`startChat:${currentUserId}, ${userId} `)
-    // res.json({ userId })[]
+    try {
+        const currentUserId = req.body.currentUserId;
+        const userId = req.body.userId
+        console.log(`startChat:${currentUserId}, ${userId} `)
+        // res.json({ userId })[]
 
-    //put users into room
+        //put users into room
 
 
+        //delete from ready users
+        const currentUser = readyUsers.findIndex((obj: { userId: any; }) => obj.userId == currentUserId);
+        readyUsers.splice(currentUser, 1);
+        const currentUser2 = readyUsers.findIndex((obj: { userId: any; }) => obj.userId == userId);
+        readyUsers.splice(currentUser2, 1);
 
-    //delete from ready users
-    const currentUser = readyUsers.findIndex((obj: { userId: any; }) => obj.userId == currentUserId);
-    readyUsers.splice(currentUser, 1);
-    const currentUser2 = readyUsers.findIndex((obj: { userId: any; }) => obj.userId == userId);
-    readyUsers.splice(currentUser2, 1);
-
-    console.log(readyUsers)
+        console.log('rd_m:', readyUsers)
+        res.status(200).json('updated array')
+    } catch (err) {
+        res.status(400).json('fail to update')
+    }
 
 
 })
 
+//
 matchRoutes.get('/geMe', async (req, res) => {
-    res.json({
-        userId: req.session['user']?.id || 1
-    })
+    try {
+        res.status(200).json({
+            userId: req.session['user']?.id || 10
+        })
+    } catch (err) {
+        res.status(400).json('fail to get current user')
+    }
+
 })
 
+//Update user location
 matchRoutes.post('/', async (req, res) => {
     try {
-        const userId = req.session['user'].id;
+        const userId = req.session['user']?.id || 10;
+        if (!userId) {
+            res.status(500).json('Please login in first')
+            return;
+        }
         const latitude = req.body.latitude;
         const longitude = req.body.longtitude;
         const location = { lat: latitude, lng: longitude }
 
         const userLocation = { userId: userId, location: location }
+
+
+
         readyUsers.push(userLocation)
 
         console.log(readyUsers)
@@ -60,10 +82,10 @@ matchRoutes.post('/', async (req, res) => {
     }
 })
 
-
+//get readyUsers Array
 matchRoutes.get('/', async (req, res) => {
     try {
-        console.log(readyUsers)
+        console.log('ru: ', readyUsers)
         res.status(200).json(readyUsers)
     } catch (err) {
         console.log(err)
